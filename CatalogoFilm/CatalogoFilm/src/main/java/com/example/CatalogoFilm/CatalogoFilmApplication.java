8fb5c8e22f1397aa4a281d50e9e885675609c906
Loto <195737806+Loto417@users.noreply.github.com>
2025-09-16 10:50:41 +0200

    Add files via upload

---
+++ CatalogoFilm/CatalogoFilm/src/main/java/com/example/CatalogoFilm/CatalogoFilmApplication.java
@@ -0,0 +1,13 @@
+package com.example.CatalogoFilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoFilmApplication.class, args);
+	}
+
+}
---
+++ CatalogoFilm/CatalogoFilm/src/test/java/com/example/CatalogoFilm/CatalogoFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CatalogoFilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

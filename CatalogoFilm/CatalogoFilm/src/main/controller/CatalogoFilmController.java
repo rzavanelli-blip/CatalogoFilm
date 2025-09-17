94254331c6547c3209f651509c119227a3adf3d7
cdilallo-beep <cdilallo@itsmeccanicabruzzo.eu>
2025-09-17 10:44:56 +0200

    Add files via upload

--- CatalogoFilm/CatalogoFilm/src/main/controller/CatalogoFilmController.java
+++ CatalogoFilm/CatalogoFilm/src/main/controller/CatalogoFilmController.java
@@ -26,6 +26,28 @@ public List<Fil> getTuttiIFilm(){
     return service.getTuttiIFilm();
 }
 
-    
+@GetMapping("/titolo")
+    public List<Film> cercaPerTitolo(@RequestParam String titolo) {
+        return service.cercaPerTitolo(titolo);
+    }
+
+    @GetMapping("/genere")
+    public List<Film> cercaPerGenere(@RequestParam String genere) {
+        return service.cercaPerGenere(genere);
+    }
+
+    @PutMapping("/{id}")
+    public String modificaFilm(@PathVariable int id, @RequestBody Film film) {
+        boolean modificato = service.modificaFilm(id, film);
+        return modificato ? "Film modificato con successo!" : "Film non trovato.";
+    }
+
+    @DeleteMapping("/{id}")
+    public String eliminaFilm(@PathVariable int id) {
+        boolean eliminato = service.eliminaFilm(id);
+        return eliminato ? "Film eliminato con successo!" : "Film non trovato.";
+    }
 }
+    
+
  
